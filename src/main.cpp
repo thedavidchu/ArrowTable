75664d2389f761367e024ba3e0734d19b9cc87a7
David Chu <thedavidchu@gmail.com>
2022-06-28 22:12:50 -0400

    Preliminary c++ implementation. Ugh, Rust is so hard to write! I'm going to try to stick closely to C

--- src/main.cpp
+++ src/main.cpp
@@ -10,20 +10,20 @@ typedef int KeyType;
 typedef int ValueType;
 
 
-struct TableItem {
+typedef struct {
     bool valid;
     size_t arrow;
 
     size_t hashcode;
     KeyType key;
     ValueType value;
-};
+} TableItem;
 
-struct Table {
+typedef struct {
     TableItem *table;
     size_t len;
     size_t cap;
-};
+} Table;
 
 size_t hash_key(KeyType key) {
     return (size_t)key;
@@ -37,6 +37,8 @@ static void _get_bounds_nocheck(Table *me, size_t home, size_t bounds[2]) {
 static size_t _get_count_nocheck(Table *me, size_t home) {
     
 }
+
+Table 
 
 bool Table_insertion(Table *me, KeyType key, ValueType value) {
     size_t hashcode = hash_key(key);
