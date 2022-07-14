0d002d1bb788843dcd68ed0060d8ef42de8a1ee7
David Chu <thedavidchu@gmail.com>
2022-07-13 21:52:50 -0400

    Sort of did insertion. Wrote search without testing

--- src/c-implementation/main.c
+++ src/c-implementation/main.c
@@ -72,10 +72,10 @@ void atbl_moveidx_nocheck(Table *me, size_t src_idx, size_t dst_idx) {
     dst->value = src->value;
 }
 
-
-
-void atbl_insert_nocheck(Table *me, KeyType key, size_t hashcode, size_t home, ValueType value) {
+void atbl_insert_knownhashcode_nocheck(Table *me, KeyType key, size_t hashcode, size_t home, ValueType value) {
     size_t empty_idx = atbl_get_firstemptyidx_nocheck(me, home);
+
+    me->table[empty_idx].arrow = NOWHERE;
 
     for (size_t table_idx = empty_idx; table_idx != home; table_idx = (table_idx - 1) % me->cap) {
         size_t count = atbl_get_count_nocheck(me, table_idx);
@@ -84,7 +84,11 @@ void atbl_insert_nocheck(Table *me, KeyType key, size_t hashcode, size_t home, V
             size_t start_idx = table_idx + me->table[table_idx].arrow;
             size_t end_idx = (table_idx + 1 + me->table[(table_idx + 1) % me->cap].arrow) % me->cap;
 
+            assert(end_idx == empty_idx && "end is not empty");
+
             atbl_moveidx_nocheck(me, start_idx, end_idx);
+
+            empty_idx = start_idx;
 
             ++me->table[end_idx].arrow;
             if (atbl_get_count_nocheck(me, (table_idx - 1) % me->cap) == 0) {
@@ -93,27 +97,28 @@ void atbl_insert_nocheck(Table *me, KeyType key, size_t hashcode, size_t home, V
         }
     }
 
-    // /* either home+1 + arrow */
-    // switch (me->table[(home + 1) % me->cap].arrow) {
-    // case EMPTY:
-    //     me->table[(home + 1)];
-    // case NOWHERE:
-    // default:
-    // }
-    // atbl_moveidx_nocheck(me, /*src*/0, empty_idx);
+    me->table[empty_idx].hashcode = hashcode;
+    me->table[empty_idx].key = key;
+    me->table[empty_idx].value = value;
 
-    // ++me->len;
+    if (me->table[home].arrow == EMPTY || me->table[home].arrow == NOWHERE) {
+        me->table[home].arrow = (empty_idx - home) % me->cap;
+    }
+    if (me->table[(home + 1) % me->cap].arrow == EMPTY) {
+        /* no operation */
+    } else if (me->table[(home + 1) % me->cap].arrow == NOWHERE) {
+        /* offset (i.e. arrow) is the same between start and end if there is
+        only one element. */
+        me->table[(home + 1) % me->cap].arrow = me->table[home].arrow;
+    } else {
+        ++me->table[(home + 1) % me->cap].arrow;
+    }
+    ++me->len;
 }
 
-bool atbl_insertion(Table *me, KeyType key, ValueType value) {
-    const size_t hashcode = key_hash(key);
-    const size_t home = hashcode % me->cap;
-    size_t offset = 0;
+bool atbl_insert_knownhashcode(
+    Table *me, KeyType key, size_t hashcode, size_t home, ValueType value) {
     size_t count = 0;
-
-    if (me->len > GROWTH_THRESHOLD * me->cap) {
-        assert(0 && "todo: allow expansion!");
-    }
 
     /* Base case */
     if (me->table[home].arrow == EMPTY) {
@@ -130,6 +135,7 @@ bool atbl_insertion(Table *me, KeyType key, ValueType value) {
     /* If there are items belonging to 'home', check for the key. */
     count = atbl_get_count_nocheck(me, home);
     if (count > 0) {
+        size_t offset = 0;
         for (offset = 0; offset < count; ++offset) {
             const size_t table_idx = (home + offset) % me->cap;
             TableItem *item_p = &me->table[table_idx];
@@ -146,10 +152,67 @@ bool atbl_insertion(Table *me, KeyType key, ValueType value) {
 
 insert:
     /* pass */
-    atbl_insert_nocheck(me, key, hashcode, home, value);
+    atbl_insert_knownhashcode_nocheck(me, key, hashcode, home, value);
 
     return false;
 }
+
+bool atbl_insert(Table *me, KeyType key, ValueType value) {
+    const size_t hashcode = key_hash(key);
+    const size_t home = hashcode % me->cap;
+
+    if (me->len >= GROWTH_THRESHOLD * me->cap) {
+        TableItem *old_table = me->table;
+        const size_t old_cap = me->cap;
+        const size_t new_cap = 2 * me->cap;
+        const size_t new_size = new_cap * sizeof(TableItem);
+        size_t i = 0;
+
+        assert(new_cap == new_size / sizeof(TableItem) && "overflow!");
+
+        me->table = malloc(new_size);
+        me->len = 0;
+        me->cap = new_cap;
+
+        for (i = 0; i < new_cap; ++i) {
+            me->table[i].arrow = EMPTY;
+        }
+
+        for (i = 0; i < old_cap; ++i) {
+            const TableItem *item = &old_table[i];
+            if (item->arrow != EMPTY) {
+                const size_t home = item->hashcode / me->cap;
+                atbl_insert_knownhashcode(
+                    me, item->key, item->hashcode, home, item->value);
+            }
+        }
+    }
+
+    return atbl_insert_knownhashcode(me, key, hashcode, home, value);
+}
+
+
+TableItem *atbl_search(Table *me, KeyType key) {
+    const size_t hashcode = key_hash(key);
+    const size_t home = hashcode % me->cap;
+    const size_t count = atbl_get_count_nocheck(me, home);
+    if (count > 0) {
+        size_t offset = 0;
+        for (offset = 0; offset < count; ++offset) {
+            const size_t table_idx = (home + offset) % me->cap;
+            TableItem *item_p = &me->table[table_idx];
+
+            if (hashcode == item_p->hashcode && key_eq(key, item_p->key)) {
+                return item_p;
+            }
+        }
+
+        goto not_found;
+    }
+not_found:
+    return NULL;
+}
+
 
 int main(void) {
     int r = 0;
@@ -158,9 +221,26 @@ int main(void) {
     r = tbl_init(&me, 10);
     assert(!r);
 
-    atbl_insertion(&me, 0, 0);
-    atbl_insertion(&me, 10, 0);
-    atbl_insertion(&me, 20, 0);
+    atbl_insert(&me, 0, 0);
+    atbl_insert(&me, 10, 0);
+    atbl_insert(&me, 20, 0);
+    atbl_insert(&me, 30, 0);
+    atbl_insert(&me, 40, 0);
+    atbl_insert(&me, 50, 0);
+    atbl_insert(&me, 60, 0);
+    atbl_insert(&me, 70, 0);
+    tbl_debug_verbose(&me);
+
+    atbl_insert(&me, 80, 0);
+    tbl_debug_verbose(&me);
+
+    atbl_insert(&me, 90, 0);
+    tbl_debug_verbose(&me);
+    atbl_insert(&me, 100, 0);
+    tbl_debug_verbose(&me);
+    atbl_insert(&me, 110, 0);
+    tbl_debug_verbose(&me);
+    atbl_insert(&me, 120, 0);
     tbl_debug_verbose(&me);
 
 
