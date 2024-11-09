b474848a132ded6d024b5cd6066a88d5d6e448cd
David Chu <thedavidchu@gmail.com>
2024-11-09 17:22:11 -0500

    Create traces with removal

--- src/generate_trace.py
+++ src/generate_trace.py
@@ -8,22 +8,29 @@ def generate_trace(seed: int, max_num_unique: int, length: int) -> list[tuple[st
             ("PUT", <key>, <value>)
             or
             ("GET", <key>, <expected-value>)
+            or
+            ("DEL", <key>, <incumbent-value>)
     """
     prng = random.Random(seed)
     trace = []
     oracle = {}
-    unique_value = 0
-    for i in range(length):
-        op = prng.randint(0, 1)
+    for unique_value in range(length):
+        op = prng.randint(0, 2)
         key = prng.randint(0, max_num_unique - 1)
         if op == 0:
             op_str = "GET"
             value = oracle.get(key, -1)
-        else:
+        elif op == 1:
             op_str = "PUT"
             value = unique_value
             oracle[key] = unique_value
             unique_value += 1
+        else:
+            op_str = "DEL"
+            # The value associated with a deletion corresponds to the
+            # incumbent value (i.e. what a 'get' would return).
+            value = oracle.get(key, -1)
+            oracle[key] = -1
         trace.append((op_str, key, value))
     return trace
 
