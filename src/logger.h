7c153e2bd8ec6cb3415b068a11a0d6729a9fe45a
David Chu <thedavidchu@gmail.com>
2024-11-09 17:13:23 -0500

    Change logger level to INFO

--- src/logger.h
+++ src/logger.h
@@ -29,7 +29,7 @@
 //      means there is no performance overhead for unused logging (assuming
 //      effective dead code elimination).
 #define LOGGER_STREAM stdout
-#define LOGGER_LEVEL  LOGGER_LEVEL_NONE
+#define LOGGER_LEVEL  LOGGER_LEVEL_INFO
 
 // NOTE The relationship between these levels is subject to change. But
 //      if you do go ahead and change them, you need to change the
